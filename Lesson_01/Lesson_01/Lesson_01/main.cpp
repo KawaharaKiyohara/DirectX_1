38ff3d3531d785b3c556fd616d83a6981043ff1c
TakayukiKiyohara <nezumimusume30@gmail.com>
2017-10-22 15:42:16 +0900

    Lesson_01を追加。

---
+++ Lesson_01/Lesson_01/Lesson_01/main.cpp
@@ -0,0 +1,198 @@
+#include <Windows.h>
+#include <d3d11.h>
+
+
+///////////////////////////////////////////////////////////////////
+// グローバル変数。
+///////////////////////////////////////////////////////////////////
+HWND					g_hWnd = NULL;				//ウィンドウハンドル。
+
+//ここからDirectX11関係の変数。
+D3D_FEATURE_LEVEL		g_featureLevel;				//Direct3D デバイスのターゲットとなる機能セット。
+ID3D11Device*			g_pd3dDevice = NULL;		//D3D11デバイス。
+IDXGISwapChain*			g_pSwapChain = NULL;		//スワップチェイン。
+ID3D11DeviceContext*	g_pd3dDeviceContext = NULL;	//D3D11デバイスコンテキスト。
+ID3D11RenderTargetView* g_backBuffer = NULL;		//バックバッファ。
+///////////////////////////////////////////////////////////////////
+// DirectXの初期化。
+///////////////////////////////////////////////////////////////////
+void InitDirectX()
+{
+	//スワップチェインを作成するための情報を設定する。
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.BufferCount = 1;									//スワップチェインのバッファ数。通常は１。
+	sd.BufferDesc.Width = 500;							//フレームバッファの幅。
+	sd.BufferDesc.Height = 500;							//フレームバッファの高さ。
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;	//フレームバッファのフォーマット。R8G8B8A8の32bit。
+	sd.BufferDesc.RefreshRate.Numerator = 60;			//モニタのリフレッシュレート。(バックバッファとフロントバッファを入れ替えるタイミングとなる。)
+	sd.BufferDesc.RefreshRate.Denominator = 1;			//２にしたら30fpsになる。1でいい。
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;	//サーフェスまたはリソースを出力レンダー ターゲットとして使用します。
+	sd.OutputWindow = g_hWnd;							//出力先のウィンドウハンドル。
+	sd.SampleDesc.Count = 1;							//1でいい。
+	sd.SampleDesc.Quality = 0;							//MSAAなし。0でいい。
+	sd.Windowed = TRUE;									//ウィンドウモード。TRUEでよい。
+
+	//利用するDirectXの機能セット。DirectX10以上に対応しているGPUを利用可能とする。
+	//この配列はD3D11CreateDeviceAndSwapChainの引数として使う。
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+	
+	//D3Dデバイスとスワップチェインを作成する。
+	D3D11CreateDeviceAndSwapChain(
+		NULL,											//NULLでいい。
+		D3D_DRIVER_TYPE_HARDWARE,						//D3Dデバイスがアクセスするドライバーの種類。
+														//基本的にD3D_DRIVER_TYPE_HARDWAREを指定すればよい。
+		NULL,											//NULLでいい。
+		0,												//０でいい。
+		featureLevels,									//D3Dデバイスのターゲットとなる機能セットを指定する。
+														//今回のサンプルはDirectX10以上をサポートするので、
+														//それらを含むD3D_FEATURE_LEVELの配列を渡す。
+		sizeof(featureLevels)/sizeof(featureLevels[0]),	//機能セットの数。
+		D3D11_SDK_VERSION,								//使用するDirectXのバージョン。
+														//D3D11_SDK_VERSIONを指定すればよい。
+		&sd,											//スワップチェインを作成するための情報。
+		&g_pSwapChain,									//作成したスワップチェインのアドレスの格納先。
+		&g_pd3dDevice,									//作成したD3Dデバイスのアドレスの格納先。
+		&g_featureLevel,								//使用される機能セットの格納先。
+		&g_pd3dDeviceContext							//作成したD3Dデバイスコンテキストのアドレスの格納先。
+	);
+
+	//書き込み先になるレンダリングターゲットを作成。
+	ID3D11Texture2D* pBackBuffer = NULL;
+	g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+	g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_backBuffer);
+	pBackBuffer->Release();
+}
+///////////////////////////////////////////////////////////////////
+// DirectXの終了処理。
+///////////////////////////////////////////////////////////////////
+void ReleaseDirectX()
+{
+	if (g_backBuffer != NULL) {
+		g_backBuffer->Release();
+	}
+	if (g_pSwapChain != NULL) {
+		g_pSwapChain->Release();
+	}
+	if (g_pd3dDeviceContext != NULL) {
+		g_pd3dDeviceContext->Release();
+	}
+	if (g_pd3dDevice != NULL) {
+		g_pd3dDevice->Release();
+	}
+}
+///////////////////////////////////////////////////////////////////
+//メッセージプロシージャ。
+//hWndがメッセージを送ってきたウィンドウのハンドル。
+//msgがメッセージの種類。
+//wParamとlParamは引数。今は気にしなくてよい。
+///////////////////////////////////////////////////////////////////
+LRESULT CALLBACK MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	//送られてきたメッセージで処理を分岐させる。
+	switch (msg)
+	{
+	case WM_DESTROY:
+		ReleaseDirectX();
+		PostQuitMessage(0);
+		break;	
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+
+	return 0;
+}
+
+///////////////////////////////////////////////////////////////////
+// ウィンドウの初期化。
+///////////////////////////////////////////////////////////////////
+void InitWindow(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	//ウィンドウクラスのパラメータを設定(単なる構造体の変数の初期化です。)
+	WNDCLASSEX wc =
+	{
+		sizeof(WNDCLASSEX),		//構造体のサイズ。
+		CS_CLASSDC,				//ウィンドウのスタイル。
+								//ここの指定でスクロールバーをつけたりできるが、ゲームでは不要なのでCS_CLASSDCでよい。
+		MsgProc,				//メッセージプロシージャ(後述)
+		0,						//0でいい。
+		0,						//0でいい。
+		GetModuleHandle(NULL),	//このクラスのためのウインドウプロシージャがあるインスタンスハンドル。
+								//何も気にしなくてよい。
+		NULL,					//アイコンのハンドル。アイコンを変えたい場合ここを変更する。とりあえずこれでいい。
+		NULL,					//マウスカーソルのハンドル。NULLの場合はデフォルト。
+		NULL,					//ウィンドウの背景色。NULLの場合はデフォルト。
+		NULL,					//メニュー名。NULLでいい。
+		TEXT("Sample_01"),		//ウィンドウクラスに付ける名前。
+		NULL					//NULLでいい。
+	};
+	//ウィンドウクラスの登録。
+	RegisterClassEx(&wc);
+
+	// ウィンドウの作成。
+	g_hWnd = CreateWindow(
+		TEXT("Sample_01"),		//使用するウィンドウクラスの名前。
+								//先ほど作成したウィンドウクラスと同じ名前にする。
+		TEXT("Sample_01"),		//ウィンドウの名前。ウィンドウクラスの名前と別名でもよい。
+		WS_OVERLAPPEDWINDOW,	//ウィンドウスタイル。ゲームでは基本的にWS_OVERLAPPEDWINDOWでいい、
+		0,						//ウィンドウの初期X座標。
+		0,						//ウィンドウの初期Y座標。
+		500,					//ウィンドウの幅。
+		500,					//ウィンドウの高さ。
+		NULL,					//親ウィンドウ。ゲームでは基本的にNULLでいい。
+		NULL,					//メニュー。今はNULLでいい。
+		hInstance,				//アプリケーションのインスタンス。
+		NULL
+	);
+
+	ShowWindow(g_hWnd, nCmdShow);
+
+}
+///////////////////////////////////////////////////////////////////
+// 毎フレーム呼ばれるゲームの更新処理。
+///////////////////////////////////////////////////////////////////
+void GameUpdate()
+{
+	float ClearColor[4] = { 0.5f, 0.5f, 0.5f, 1.0f }; //red,green,blue,alpha
+	//描き込み先をバックバッファにする。
+	g_pd3dDeviceContext->OMSetRenderTargets(1, &g_backBuffer, NULL);
+	//バックバッファを灰色で塗りつぶす。
+	g_pd3dDeviceContext->ClearRenderTargetView(g_backBuffer, ClearColor);
+
+	/////////////////////////////////////////////////
+	// ここに3Dモデルなどを描画するコードを書いていく。
+	/////////////////////////////////////////////////
+
+	//バックバッファとフロントバッファを入れ替える。
+	g_pSwapChain->Present(0, 0);
+}
+///////////////////////////////////////////////////////////////////
+// ウィンドウプログラムのメイン関数。
+///////////////////////////////////////////////////////////////////
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	//ウィンドウの初期化。
+	InitWindow(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+	//DirectXの初期化。
+	InitDirectX();
+	//メッセージ構造体の変数msgを初期化。
+	MSG msg = { 0 };
+	while (WM_QUIT != msg.message)	//メッセージループ
+	{
+		//ウィンドウからのメッセージを受け取る。
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			//ウィンドウからのメッセージがないので、ゲームの処理を実行する。
+			GameUpdate();
+		}
+	}
+}
