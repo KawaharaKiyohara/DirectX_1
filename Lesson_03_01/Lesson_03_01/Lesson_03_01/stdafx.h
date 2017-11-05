c93fcd5708d28e2437c664cb4956cb19923ba733
TakayukiKiyohara <nezumimusume30@gmail.com>
2017-11-05 14:42:20 +0900

    インデックスバッファの教材を追加。

---
+++ Lesson_03_01/Lesson_03_01/Lesson_03_01/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory>
+#include <D3Dcompiler.h>
+#include <Windows.h>
+#include <d3d11.h>
+
+extern ID3D11Device* g_pd3dDevice ;			//D3D11デバイス。
+extern ID3D11DeviceContext* g_pd3dDeviceContext;	//D3D11デバイスコンテキスト。
