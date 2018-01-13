197889491a75572ba0fa2bc90a60988bc3889a54
TakayukiKiyohara <nezumimusume30@gmail.com>
2018-01-14 08:40:59 +0900

    行列の教材を追加

---
+++ Lesson_12_01_Answer/Lesson_12/Lesson_12/main.cpp
@@ -0,0 +1,193 @@
+#include "stdafx.h"
+#include "Effect.h"
+
+#include "Sprite.h"	//Spriteクラスを使用するので、ヘッダーをインクルード。
+#include "SkinModel.h" //SkinModelクラスを使用するので、ヘッダーをインクルード。
+
+///////////////////////////////////////////////////////////////////
+// グローバル変数。
+///////////////////////////////////////////////////////////////////
+HWND			g_hWnd = NULL;				//ウィンドウハンドル。
+CCamera* g_camera3D = NULL;					//3Dカメラ。
+GraphicsEngine* g_graphicsEngine = NULL;	//グラフィックスエンジン。
+
+
+//ティーポット関係の変数。
+CVector3 g_teapotPosition = CVector3::Zero();			//ティーポットの座標。
+CQuaternion g_teapotRotation = CQuaternion::Identity();	//ティーポットの回転。
+CVector3 g_teapotScale = CVector3::One();				//ティーポットの拡大率。
+SkinModel g_teapotModel;								//ティーポットモデル。
+
+///////////////////////////////////////////////////////////////////
+// DirectXの終了処理。
+///////////////////////////////////////////////////////////////////
+void ReleaseDirectX()
+{
+	delete g_camera3D;
+	delete g_graphicsEngine;
+	
+}
+///////////////////////////////////////////////////////////////////
+//メッセージプロシージャ。
+//hWndがメッセージを送ってきたウィンドウのハンドル。
+//msgがメッセージの種類。
+//wParamとlParamは引数。今は気にしなくてよい。
+///////////////////////////////////////////////////////////////////
+LRESULT CALLBACK MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	//送られてきたメッセージで処理を分岐させる。
+	switch (msg)
+	{
+	case WM_DESTROY:
+		ReleaseDirectX();
+		PostQuitMessage(0);
+		break;	
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+
+	return 0;
+}
+
+///////////////////////////////////////////////////////////////////
+// ウィンドウの初期化。
+///////////////////////////////////////////////////////////////////
+void InitWindow(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	//ウィンドウクラスのパラメータを設定(単なる構造体の変数の初期化です。)
+	WNDCLASSEX wc =
+	{
+		sizeof(WNDCLASSEX),		//構造体のサイズ。
+		CS_CLASSDC,				//ウィンドウのスタイル。
+								//ここの指定でスクロールバーをつけたりできるが、ゲームでは不要なのでCS_CLASSDCでよい。
+		MsgProc,				//メッセージプロシージャ(後述)
+		0,						//0でいい。
+		0,						//0でいい。
+		GetModuleHandle(NULL),	//このクラスのためのウインドウプロシージャがあるインスタンスハンドル。
+								//何も気にしなくてよい。
+		NULL,					//アイコンのハンドル。アイコンを変えたい場合ここを変更する。とりあえずこれでいい。
+		NULL,					//マウスカーソルのハンドル。NULLの場合はデフォルト。
+		NULL,					//ウィンドウの背景色。NULLの場合はデフォルト。
+		NULL,					//メニュー名。NULLでいい。
+		TEXT("Sample_01"),		//ウィンドウクラスに付ける名前。
+		NULL					//NULLでいい。
+	};
+	//ウィンドウクラスの登録。
+	RegisterClassEx(&wc);
+
+	// ウィンドウの作成。
+	g_hWnd = CreateWindow(
+		TEXT("Sample_01"),		//使用するウィンドウクラスの名前。
+								//先ほど作成したウィンドウクラスと同じ名前にする。
+		TEXT("Sample_01"),		//ウィンドウの名前。ウィンドウクラスの名前と別名でもよい。
+		WS_OVERLAPPEDWINDOW,	//ウィンドウスタイル。ゲームでは基本的にWS_OVERLAPPEDWINDOWでいい、
+		0,						//ウィンドウの初期X座標。
+		0,						//ウィンドウの初期Y座標。
+		(UINT)FRAME_BUFFER_W,	//ウィンドウの幅。
+		(UINT)FRAME_BUFFER_H,	//ウィンドウの高さ。
+		NULL,					//親ウィンドウ。ゲームでは基本的にNULLでいい。
+		NULL,					//メニュー。今はNULLでいい。
+		hInstance,				//アプリケーションのインスタンス。
+		NULL
+	);
+
+	ShowWindow(g_hWnd, nCmdShow);
+
+}
+
+
+///////////////////////////////////////////////////////////////////
+// 毎フレーム呼ばれるゲームの更新処理。
+///////////////////////////////////////////////////////////////////
+void GameUpdate()
+{
+	
+	g_graphicsEngine->BegineRender();
+	//10.2.5 Hands-On　①　平行移動行列を作って、ティーポットを動かす。
+	/*if (GetAsyncKeyState(VK_LEFT)) {
+		g_teapotPosition.x -= 2.0f;
+	}
+	if (GetAsyncKeyState(VK_RIGHT)) {
+		g_teapotPosition.x += 2.0f;
+	}
+	CMatrix mTrans;
+	mTrans.MakeTranslation(g_teapotPosition);
+	g_teapotModel.m_worldMatrix = mTrans;*/
+	//10.2.5 Hands-On　②　回転行列を作って、ティーポットを回転させる。
+	/*CQuaternion qAddRot = CQuaternion::Identity();
+	if (GetAsyncKeyState('A')) {
+		qAddRot.SetRotationDeg(CVector3::AxisY(), 2.0f);
+	}
+	if (GetAsyncKeyState('D')) {
+		qAddRot.SetRotationDeg(CVector3::AxisY(), -2.0f);
+	}
+	g_teapotRotation.Multiply(qAddRot);
+	CMatrix mRot;
+	mRot.MakeRotationFromQuaternion(g_teapotRotation);
+	g_teapotModel.m_worldMatrix = mRot;*/
+	//10.2.5 Hands-On　③　拡大行列を作って、ティーポットを拡大させる。
+	/*if (GetAsyncKeyState(VK_UP)) {
+		g_teapotScale.y += 0.1f;
+	}
+	if (GetAsyncKeyState(VK_DOWN)) {
+		g_teapotScale.y -= 0.1f;
+	}
+	CMatrix mScale;
+	mScale.MakeScaling(g_teapotScale);
+	g_teapotModel.m_worldMatrix = mScale;*/
+	//10.2.7 Hands-On  ①　平行移動行列と回転行列を乗算して、ティーポットを動かす。
+	
+	//10.2.7 Hands-On  ②　平行移動と回転の乗算順番を変更すると
+	//3Dモデルを描画する。
+	
+	
+	g_teapotModel.Draw(g_camera3D->GetViewMatrix(), g_camera3D->GetProjectionMatrix());
+
+	g_graphicsEngine->EndRender();
+	
+}
+
+///////////////////////////////////////////////////////////////////
+// カメラを初期化。
+///////////////////////////////////////////////////////////////////
+void InitCamera()
+{
+	//3Dカメラを初期化。
+	g_camera3D = new CCamera;
+	g_camera3D->SetTarget({0.0f, 50.0f, 0.0f});			//注視点は原点。
+	g_camera3D->SetPosition({ 0.0f, 50.0f, -500.0f });	//視点を設定。
+	g_camera3D->Update();
+
+}
+///////////////////////////////////////////////////////////////////
+// ウィンドウプログラムのメイン関数。
+///////////////////////////////////////////////////////////////////
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	//ウィンドウの初期化。
+	InitWindow(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+	//DirectXの初期化。
+	g_graphicsEngine = new GraphicsEngine();
+	g_graphicsEngine->Init(g_hWnd);
+	//カメラを初期化。
+	InitCamera();
+	
+
+	//ティーポットモデルを初期化。
+	g_teapotModel.Load(L"modelData/teapot.cmo");
+	//メッセージ構造体の変数msgを初期化。
+	MSG msg = { 0 };
+	while (WM_QUIT != msg.message)	//メッセージループ
+	{
+		//ウィンドウからのメッセージを受け取る。
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			//ウィンドウからのメッセージがないので、ゲームの処理を実行する。
+			GameUpdate();
+		}
+	}
+}
